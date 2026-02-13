31610a3a2742753e44cc304a83c7fd5b19c61f13
Dunridge <Polinka7max@gmail.com>
2026-02-12 20:35:31 -0800

    add favicon

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,12 @@
 export default function Header() {
   return (
     <header
-      className="w-full flex items-center text-white py-6 px-6"
+      className="w-full flex items-center text-white py-6 px-6 gap-4"
       style={{
-        background:
-          "linear-gradient(rgb(36, 118, 255) 0%, rgb(14, 84, 201) 100%)",
+        background: "rgb(245, 245, 245)",
       }}
     >
-      <div className="text-lg font-bold">Uplane</div>
+      <img src="/uplane-logo.svg" alt="Uplane Logo" />
     </header>
   );
 }
